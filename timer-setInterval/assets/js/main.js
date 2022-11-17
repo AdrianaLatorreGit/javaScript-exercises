d9dac4058675283a2e3836f95e562c640bec37bc
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-11-17 11:01:47 -0300

    Add files via upload

---
+++ timer-setInterval/assets/js/main.js
@@ -0,0 +1,39 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC',
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function (e) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+
+pausar.addEventListener('click', function (e) {
+    clearInterval(timer);
+    relogio.classList.add('pausado');
+});
+
+zerar.addEventListener('click', function (e) {
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
